a31bd4255ceb6e0db33d27065c24302047d9e307
vladinagdimaev <140087083+vladinagdimaev@users.noreply.github.com>
2026-08-02 11:46:43 +0300

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -1,4 +1,9 @@
-from pelicanconf import *
+from pathlib import Path
+
+conf = {}
+exec((Path(__file__).parent / "pelicanconf.py").read_text(), conf)
+
+globals().update(conf)
 
 SITEURL = "https://sensei.art"
 RELATIVE_URLS = False
