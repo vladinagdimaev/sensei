39dc3d05c5253db7b36d937608f1a1f29949171c
vladinagdimaev <140087083+vladinagdimaev@users.noreply.github.com>
2026-08-04 08:25:39 +0000

    Add public coming soon page

---
+++ comingsoonconf.py
@@ -0,0 +1,9 @@
+from pelicanconf import *
+
+# האתר הציבורי הזמני כולל רק את דף ה־Coming Soon.
+DIRECT_TEMPLATES = []
+PAGE_PATHS = []
+
+TEMPLATE_PAGES = {
+    "coming-soon.html": "index.html",
+}
